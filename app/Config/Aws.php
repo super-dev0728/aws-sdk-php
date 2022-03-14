78cea6b8bf97b8cb8200599e6dd3d48c3519861b
super-dev0728 <djtonyregent@gmail.com>
2022-03-14 17:22:05 +0300

    ...

--- app/Config/Aws.php
+++ app/Config/Aws.php
@@ -4,6 +4,6 @@ namespace Config;
 
 class Aws
 {
-    public $aws_access_key = "AKIAVCG7MSHB6JFWVSW4";
-    public $aws_secret_key = "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";
+    public $aws_access_key = "";
+    public $aws_secret_key = "";
 }
